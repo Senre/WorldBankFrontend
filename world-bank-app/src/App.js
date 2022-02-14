ce5c6160d0b073f52d0f6a750f9f5ad5368634ac
Mariam Raashid <milylily_m@hotmail.com>
2022-02-14 17:41:22 +0000

    routing done

--- world-bank-app/src/App.js
+++ world-bank-app/src/App.js
@@ -3,40 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Register from "./Register";
 import LoginPage from "./Components/LoginPage";
+import { Route, Redirect, Switch } from "react-router-dom";
+import SearchPage from "./Components/SearchPage";
+import Register from "./Register";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      emailInput: "",
-      passwordInput: "",
-    };
-  }
-
-  async postUser(email, password) {
-    const endpoint = "http://localhost:8080/";
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    });
-    const json = response.json();
-    return json;
-  }
-
   render() {
     return (
-      <div className="App">
-        <Register
-          registerUser={(email, password) => this.postUser(email, password)}
-        />
-        <LoginPage />
-      </div>
+      <Switch>
+        <Route path="/home">
+          <SearchPage />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/register" />
+        </Route>
+      </Switch>
     );
   }
 }
