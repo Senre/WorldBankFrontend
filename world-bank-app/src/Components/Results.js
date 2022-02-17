27498b9bac007c4bc5a6d17920f4d5d76af37ef8
Mariam01010010 <94632221+Mariam01010010@users.noreply.github.com>
2022-02-17 16:27:51 +0000

    Update Results.js

--- world-bank-app/src/Components/Results.js
+++ world-bank-app/src/Components/Results.js
@@ -1,103 +1,154 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Register from "./Components/Register";
-import LoginPage from "./Components/LoginPage";
-import Results from "./Components/Results";
-import SearchPage from "./Components/SearchPage";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { withCookies, Cookies } from "react-cookie";
-import { instanceOf } from "prop-types";
+import Graph from "./Graph";
+import Header from "./Header";
 
-class App extends React.Component {
-  static propTypes = {
-    cookies: instanceOf(Cookies).isRequired,
-  };
-
+class Results extends React.Component {
   constructor(props) {
     super(props);
-    const { cookies } = props;
     this.state = {
-      results: [],
-      compareResults: [],
-      isLoggedIn: cookies.get("sessionId") ? true : false,
-      username: null,
+      data: this.props.data,
+      compareData: this.props.compareData,
+      comparison: this.props.compareData.length > 0,
     };
   }
 
-  setData = (data, compareData) => {
-    console.log("changed");
-    this.setState({
-      results: data ? [...data] : [],
-      compareResults: compareData ? [...compareData] : [],
+  sortIncomingData = (data) => {
+    let set = -1;
+    let sortedData = [];
+    let checkIndicator = "";
+    data.forEach((element) => {
+      if (checkIndicator !== element.indicatorname) {
+        set++;
+        sortedData.push([]);
+      }
+
+      sortedData[set].push(element);
+      checkIndicator = element.indicatorname;
     });
+
+    sortedData = sortedData.map((set) => {
+      return set.sort((a, b) => (a.year < b.year ? -1 : 1));
+    });
+
+    sortedData.sort(function (a, b) {
+      return a[0].indicatorname > b[0].indicatorname ? 1 : -1;
+    });
+
+    return sortedData;
   };
 
-  logIn = (username) => {
-    const { cookies } = this.props;
-    console.log("logged");
-    const currentState = this.state.isLoggedIn;
-    if (this.state.isLoggedIn) {
-      console.log("removed");
-      cookies.remove("sessionId");
-      cookies.remove("user_id");
-      cookies.remove("email");
-      this.setState({ isLoggedIn: !currentState, username: "" });
-    } else {
-      this.setState({ isLoggedIn: true, user: username });
+  renderLineChart = (data) => {
+    if (data.length > 2) {
+      let altData = data.map((item) => {
+        item.value = Number(item.value);
+        return item;
+      });
+      return <Graph data={altData} />;
+    } else if (data.length === 1) {
+      return (
+        <h3>
+          {data[0].indicatorname} for {data[0].countryname} was{" "}
+          {Number(data[0].value).toLocaleString()} in {data[0].year}
+        </h3>
+      );
     }
-    console.log(cookies.getAll());
+  };
+
+  renderComparison = (data, compareData) => {
+    if (data.length > 2) {
+      let altData = data.map((item) => {
+        item.value = Number(item.value);
+        return item;
+      });
+      let altCompareData = compareData.map((item) => {
+        item.value = Number(item.value);
+        return item;
+      });
+      return <Graph data={altData} compareData={altCompareData} />;
+    } else if (data.length === 1) {
+      return (
+        <div>
+          <h3>
+            {data[0].indicatorname} for {data[0].countryname} was{" "}
+            {Number(data[0].value).toLocaleString()} in {data[0].year}
+          </h3>
+          <h3>
+            {compareData[0].indicatorname} for {compareData[0].countryname} was{" "}
+            {Number(compareData[0].value).toLocaleString()} in{" "}
+            {compareData[0].year}
+          </h3>
+        </div>
+      );
+    }
+  };
+
+  filterTwoDataSets = (setOne, setTwo) => {
+    const setTwoIndicators = setTwo.map((entry) => {
+      return entry[0].indicatorname;
+    });
+
+    const setOneIndicators = setOne.map((entry) => {
+      return entry[0].indicatorname;
+    });
+
+    const filteredSetOne = setOne.filter((entry) =>
+      setTwoIndicators.includes(entry[0].indicatorname)
+    );
+
+    const filteredSetTwo = setTwo.filter((entry) =>
+      setOneIndicators.includes(entry[0].indicatorname)
+    );
+
+    return { dataMain: filteredSetOne, dataCompare: filteredSetTwo };
+  };
+
+  renderIncomingData = () => {
+    const { data, compareData, comparison } = this.state;
+    const sortedData = this.sortIncomingData(data);
+    if (comparison) {
+      const sortedCompareData = this.sortIncomingData(compareData);
+
+      const { dataMain, dataCompare } = this.filterTwoDataSets(
+        sortedData,
+        sortedCompareData
+      );
+
+      return dataMain.map((set, i) => {
+        return (
+          <div key={i} className="rendered-data">
+            {this.renderComparison(set, dataCompare[i])}
+          </div>
+        );
+      });
+    } else {
+      return sortedData.map((set, i) => {
+        return (
+          <div key={i} className="rendered-data">
+            {this.renderLineChart(set)}
+          </div>
+        );
+      });
+    }
   };
 
   render() {
+    const { data, compareData, comparison } = this.state;
     return (
-      <Switch>
-        <Route path="/home">
-          {this.state.results.length === 0 ? (
-            <SearchPage
-              setData={(data, compareData) => this.setData(data, compareData)}
-              logIn={() => this.logIn()}
-            />
-          ) : (
-            <Redirect to="/results" />
-          )}
-        </Route>
-        <Route path="/register">
-          {this.state.isLoggedIn ? (
-            <Redirect to="/home" />
-          ) : (
-            <Register logIn={(username) => this.logIn(username)} />
-          )}
-        </Route>
-        <Route path="/login" component={LoginPage}>
-          {this.state.isLoggedIn ? (
-            <Redirect to="/home" />
-          ) : (
-            <LoginPage logIn={() => this.logIn()} />
-          )}
-        </Route>
-        <Route path="/results">
-          {this.state.results.length === 0 ? (
-            <Redirect to="/home" />
-          ) : (
-            <Results
-              data={this.state.results}
-              compareData={this.state.compareResults}
-              setData={() => this.setData()}
-              logIn={() => this.logIn()}
-            />
-          )}
-        </Route>
-        <Route path="/">
-          {this.state.isLoggedIn ? (
-            <Redirect to="/home" />
-          ) : (
-            <Redirect to="/login" />
-          )}
-        </Route>
-      </Switch>
+      <div className="spacing">
+        <main>
+          <header className="main-header">
+            <Header setData={() => this.props.setData()} />
+          </header>
+          <h1 className="results-title">
+            {comparison
+              ? `${data[0].countryname} Vs. ${compareData[0].countryname}`
+              : data[0].countryname}
+          </h1>
+          <div className="results-content">{this.renderIncomingData()}</div>
+        </main>
+      </div>
     );
   }
 }
 
-export default withCookies(App);
+export default Results;
