bf8a8bed0a5d4276a3b6c969ecb760b7366e62fa
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-17 22:42:17 +0000

    login page css almost complete

--- world-bank-app/src/Components/LoginPage/LoginPage.js
+++ world-bank-app/src/Components/LoginPage/LoginPage.js
@@ -51,48 +51,55 @@ class LoginPage extends React.Component {
 
   loginPopup() {
     return (
-      <div className="spacing">
-        <div className="loginPage">
-          {/* <div>
-            <h3>Please login to access and search the World Bank Database</h3>
-          </div> */}
-          <div>
-            <Form className="form" onSubmit={(e) => this.handleSubmit(e)}>
-              <Form.Group className="mb-3">
-                <Form.Label className="username-label">Username</Form.Label>
-                <Form.Control
-                  className="username-input"
-                  type="text"
-                  placeholder="Enter email"
-                  value={this.state.email}
-                  id="email"
-                  onChange={(e) => this.handleChange(e)}
-                />
-              </Form.Group>
+      <div>
+        <header className="spacing">
+          <h1>Welcome to the World Bank Dashboard!</h1>
+          <img src="http://www.worldbank.org/" alt="WBD logo"></img>
+        </header>
+        <div className="spacing">
+          <h5>Please login to search through the World Bank Database.</h5>
+        </div>
+        <div className="spacing">
+          <div className="loginPage">
+            <div></div>
+            <div>
+              <Form className="form" onSubmit={(e) => this.handleSubmit(e)}>
+                <Form.Group className="mb-3">
+                  <Form.Label className="username-label">Username</Form.Label>
+                  <Form.Control
+                    className="username-input"
+                    type="text"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    id="email"
+                    onChange={(e) => this.handleChange(e)}
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label className="password-label">Password</Form.Label>
-                <Form.Control
-                  className="password-input"
-                  type="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  id="password"
-                  onChange={(e) => this.handleChange(e)}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-              <Form.Group className="mb-3" id="register-redirect">
-                <Form.Label className="register-label">
-                  Don't have an account?
-                </Form.Label>
-                <Link to="/register">
-                  <Button variant="primary">Create an Account</Button>
-                </Link>
-              </Form.Group>
-            </Form>
+                <Form.Group className="mb-3">
+                  <Form.Label className="password-label">Password</Form.Label>
+                  <Form.Control
+                    className="password-input"
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    id="password"
+                    onChange={(e) => this.handleChange(e)}
+                  />
+                </Form.Group>
+                <Button className="example" variant="primary" type="submit">
+                  Submit
+                </Button>
+                <Form.Group className="mb-3" id="register-redirect">
+                  <Form.Label className="register-label">
+                    Don't have an account?
+                  </Form.Label>
+                  <Link to="/register">
+                    <Button variant="primary">Create an Account</Button>
+                  </Link>
+                </Form.Group>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
